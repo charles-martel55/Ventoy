204943bc375e33deede8caabc008308777a7d1b8
charles-martel55 <crashlord@wanadoo.fr>
2024-08-02 14:12:50 +0200

    add PATH.

--- DOC/prepare_env.sh
+++ DOC/prepare_env.sh
@@ -7,3 +7,5 @@
 [ -d /opt/aarch64--uclibc--stable-2020.08-1 ] || tar xf /opt/aarch64--uclibc--stable-2020.08-1.tar.bz2  -C /opt
 
 [ -d /opt/mips-loongson-gcc7.3-linux-gnu ] || tar xf /opt/mips-loongson-gcc7.3-2019.06-29-linux-gnu.tar.gz  -C /opt
+
+export PATH=$PATH:/opt/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin:/opt/aarch64--uclibc--stable-2020.08-1/bin:/opt/mips-loongson-gcc7.3-linux-gnu/2019.06-29/bin/:/opt/mips64el-linux-musl-gcc730/bin/
